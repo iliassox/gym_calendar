b935e93ee71f467bfb73ab0c2fb040c73aca7d56
iliassox <ilyesfr154@gmail.com>
2022-03-27 21:29:09 +0000

    finishing controllers

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rooms = Room::all();
+
+        return view('pannel.room.index',compact('rooms'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pannel.room.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Room::create([
+            'capacity'=>$request->capacity
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $room = Room::find($id);
+
+        return view('pannel.coach.edit',compact('room'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $room = Room::find($id);
+
+        $room->update([
+            'capacity'=>$request->capacity
+        ]);
+
+        return redirect()->route('pannel.room.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Room::find($id)->delete();
+
+        return redirect()->route('pannel.room.index');
+    }
+}
---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Session;
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sessions = Session::all();
+
+        return view('pannel.session.index' , compact('sessions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pannel.session.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Session::create([
+            'date'=>$request->date,
+            'time'=>$request->time,
+            'coach_id'=>$request->coachID,
+            'room_id'=>$request->roomID,
+            'activity_id'=>$request->activityID
+        ]);
+
+        return redirect()->route('pannel.session.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $session = Session::find($id);
+
+        return view('pannel.session.create', compact('session'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $session = Session::find($id);
+
+        $session->update([
+            'date'=>$request->date,
+            'time'=>$request->time,
+            'coach_id'=>$request->coachID,
+            'room_id'=>$request->roomID,
+            'activity_id'=>$request->activityID
+        ]);
+
+        return redirect()->route('pannel.session.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Session::find($id)->delete();
+
+        return redirect()->route('pannel.session.index');
+    }
+}
